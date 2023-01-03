9c8aa01fde65ccda7a515e31a338ba35f3f7524b
avnercaleb <avnercaleb@hotmail.com>
2023-01-03 20:42:07 -0300

    Adicionado balanceamento de carga com Ribbon

--- hr-worker/src/main/java/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/hrworker/controller/WorkerController.java
@@ -2,7 +2,10 @@ package hrworker.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,11 @@ import hrworker.service.WorkerService;
 @RequestMapping("/workers")
 public class WorkerController {
 	
+	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private WorkerService service;
 	
@@ -27,6 +35,8 @@ public class WorkerController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
 	}
 }
